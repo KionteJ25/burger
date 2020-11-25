12bcbb824542f8da7b04902cc9e3300e4fddfb60
Kionte Jones <jkionte20@gmail.com>
2020-11-24 19:11:11 -0500

    modifications made to orm, controller, schema, index.handlebars and server.js

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,7 +18,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "name"
   ], [
     req.body.name, req.body.sleepy
   ], function(result) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,12 @@
 DROP DATABASE IF EXISTS burgers_db;
 
 CREATE DATABASE burgers_db;
+
+USE burgers_db;
   
 CREATE TABLE burgers (
     id INT NOT NULL
-    burger_name VARCHAR(50),
+    burger_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT TRUE,
+    PRIMARY KEY (id)
 );
